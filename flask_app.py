ae3a79fc04bf0aa9a5664e103330b13d0df2bdbd
divyag9 <divyagorantla@24dcaea7d349>
2018-07-16 22:01:31 +0000

    Adding new logic to display all orders in a table along with buttons to send sms and confirm pickup

--- flask_app.py
+++ flask_app.py
@@ -11,10 +11,10 @@ mysql = MySQL()
 app = Flask(__name__)
 app.config['DEBUG'] = True
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] =  os.environ['MYSQL_DATABASE_USER']
-app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
-app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
-app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+app.config['MYSQL_DATABASE_USER'] =  'divyagorantla' #os.environ['MYSQL_DATABASE_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = 'divemmhea' #os.environ['MYSQL_DATABASE_PASSWORD'] #
+app.config['MYSQL_DATABASE_DB'] = 'divyagorantla$waittimes' #os.environ['MYSQL_DATABASE_DB'] #
+app.config['MYSQL_DATABASE_HOST'] = 'divyagorantla.mysql.pythonanywhere-services.com' #os.environ['MYSQL_DATABASE_HOST'] #
 mysql.init_app(app)
 
 # logging config
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 # set twilio client
 proxy_client = TwilioHttpClient()
-proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+proxy_client.session.proxies = {'https': 'http://proxy.server:3128'}
 
 # send message using the twilio account
 def send_message(cell_phone_number, message):
@@ -41,32 +41,73 @@ def get_order_information(order_number):
     cursor.execute('SELECT * FROM ORDERS WHERE ORDER_NUMBER=%s',(order_number))
     return conn, cursor
 
+# get all orders whose pickup time is null
+def get_orders(conn, cursor):
+    cursor.execute('SELECT * FROM ORDERS WHERE PICKUP_TIME IS NULL')
+    return cursor.fetchall()
+
 @app.route('/')
 def main():
-    return render_template('index.html')
+    orders = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        orders = get_orders(conn, cursor)
+    except Exception as e:
+        logging.error(e)
+        return render_template('index.html', error=str(e), orders=orders)
+    finally:
+        cursor.close()
+        conn.close()
+
+    return render_template('index.html', orders=orders)
+
+@app.route('/createorder', methods=['POST'])
+def create_order():
+    order_number = request.form['ordernumber']
+    cell_phone_number = request.form['cellphonenumber']
+    orders = None
+    try:
+
+        conn, cursor = get_order_information(order_number)
+        row_count = cursor.rowcount
+        #send back orders on each render
+        orders = get_orders(conn, cursor)
+        # insert the order information into database if there is no record for that ordernumber else redirect to home page with an error message
+        if row_count > 0:
+            return render_template('index.html', error='Order number: %s already exists' %(order_number), orders=orders)
+        else:
+            cursor.execute('INSERT INTO ORDERS (ORDER_NUMBER, CELL_PHONE_NUMBER) VALUES (%s, %s)', (order_number,cell_phone_number))
+            conn.commit()
+    except Exception as e:
+        logging.error(e)
+        return render_template('index.html', error=str(e), orders=orders)
+    finally:
+        orders = get_orders(conn, cursor)
+        cursor.close()
+        conn.close()
+
+    return render_template('index.html', message='Order Created', orders=orders)
 
 @app.route('/sendsms', methods=['POST'])
 def send_sms():
-    order_number = request.form['ordernumber']
-    cell_phone_number = request.form['cellphonenumber']
-
+    order_number = request.form['sms_ordernumber']
+    print('in send_sms: ',order_number)
     try:
         conn, cursor = get_order_information(order_number)
-        row_count = cursor.rowcount
-        # insert the order information into database if there is no record for that ordernumber else redirect to home page with an error message
-        if row_count > 0:
-            return render_template('index.html', error='Message already sent for order number: %s' %(order_number))
-        else:
-            # when the message is not sent for that record the message_sent_time will be set to null
-            message_sent_time = None
-            random_number = randint(0, 100)
-            # send message only if the random number is even
-            if (random_number % 2) == 0:
-                send_message(cell_phone_number, 'Thank you for ordering from Haldi!  Your order will be ready to be picked up in 15 minutes.')
-                now = datetime.now()
-                message_sent_time = now.strftime('%Y-%m-%d %H:%M:%S')
-            cursor.execute('INSERT INTO ORDERS (ORDER_NUMBER, CELL_PHONE_NUMBER, MESSAGE_SENT_TIME) VALUES (%s, %s, %s)', (order_number,cell_phone_number, message_sent_time))
-            conn.commit()
+        row = cursor.fetchone()
+        cell_phone_number = row[2]
+
+        # when the message is not sent for that record the message_sent_time will be set to null
+        message_sent_time = None
+        random_number = randint(0, 100)
+        # send message only if the random number is even
+        if (random_number % 2) == 0:
+            send_message(cell_phone_number, 'Thank you for ordering from Haldi! Your order will be ready to be picked up in 15 minutes.')
+            now = datetime.now()
+            message_sent_time = now.strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute('UPDATE ORDERS SET MESSAGE_SENT_TIME = %s WHERE ORDER_NUMBER= %s', (message_sent_time,order_number))
+        conn.commit()
     except Exception as e:
         logging.error(e)
         return render_template('index.html', error=str(e))
@@ -78,23 +119,15 @@ def send_sms():
 
 @app.route('/confirmpickup', methods=['POST'])
 def confirm_pickup():
-    order_number = request.form['ordernumber']
+    order_number = request.form['pickup_ordernumber']
 
     try:
         conn, cursor = get_order_information(order_number)
-        row_count = cursor.rowcount
         row = cursor.fetchone()
-        # update the order with pickuptime if the ordernumber already exists else redirect to home page with an error message
-        if row_count == 0:
-            return render_template('index.html', error="Ordernumber: %s  doesn't exist, message was not sent previously" %(order_number))
-        # if the pickup time is already updated don't update again.
-        elif row[4] is not None:
-            return render_template('index.html', error="Already confirmed pickup for ordernumber: %s" %(order_number))
-        else:
-            now = datetime.now()
-            pickup_time = now.strftime('%Y-%m-%d %H:%M:%S')
-            cursor.execute('UPDATE ORDERS SET PICKUP_TIME = %s WHERE ORDER_NUMBER= %s', (pickup_time,order_number))
-            conn.commit()
+        now = datetime.now()
+        pickup_time = now.strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute('UPDATE ORDERS SET PICKUP_TIME = %s WHERE ORDER_NUMBER= %s', (pickup_time,order_number))
+        conn.commit()
         # once pickedup send survey question
         cell_phone_number = row[2]
         send_message(cell_phone_number, 'We hope you enjoyed your meal from Haldi! Please help us improve our service by rating your take-out order experience on a scale of 1-5, with 5 being the most positive.')
