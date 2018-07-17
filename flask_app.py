7c467338799bf4000a7451c3cd737c65d5e9e325
divyag9 <divyagorantla@24dcaea7d349>
2018-07-17 18:06:43 +0000

    Adding the logic to get orders even on exception and refractored the code

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] =  os.environ['MYSQL_DATABASE_USER']
-app.config['MYSQL_DATABASE_PASSWORD'] = ['MYSQL_DATABASE_PASSWORD']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
 app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
 app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
 mysql.init_app(app)
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 # set twilio client
 proxy_client = TwilioHttpClient()
-proxy_client.session.proxies = {'https': os.environ['http_proxy']}
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
 # send message using the twilio account
 def send_message(cell_phone_number, message):
@@ -39,39 +39,45 @@ def get_order_information(order_number):
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM ORDERS WHERE ORDER_NUMBER=%s',(order_number))
+
     return conn, cursor
 
 # get all orders whose pickup time is null
 def get_orders(conn, cursor):
     cursor.execute('SELECT * FROM ORDERS WHERE PICKUP_TIME IS NULL')
+
     return cursor.fetchall()
 
 @app.route('/')
 def main():
     orders = None
+
+    # display orders that are not completed yet(that is pick up is not confirmed)
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
         orders = get_orders(conn, cursor)
+
+        return render_template('index.html', orders=orders)
     except Exception as e:
         logging.error(e)
+
         return render_template('index.html', error=str(e), orders=orders)
     finally:
         cursor.close()
         conn.close()
-
-    return render_template('index.html', orders=orders)
 
 @app.route('/createorder', methods=['POST'])
 def create_order():
     order_number = request.form['ordernumber']
     cell_phone_number = request.form['cellphonenumber']
     orders = None
+
     try:
 
         conn, cursor = get_order_information(order_number)
         row_count = cursor.rowcount
-        #send back orders on each render
+        #send back incomplete orders on each render
         orders = get_orders(conn, cursor)
         # insert the order information into database if there is no record for that ordernumber else redirect to home page with an error message
         if row_count > 0:
@@ -79,25 +85,30 @@ def create_order():
         else:
             cursor.execute('INSERT INTO ORDERS (ORDER_NUMBER, CELL_PHONE_NUMBER) VALUES (%s, %s)', (order_number,cell_phone_number))
             conn.commit()
+        #send back incomplete orders
+        orders = get_orders(conn, cursor)
+
+        return render_template('index.html', message='Order Created', orders=orders)
     except Exception as e:
         logging.error(e)
+        #send back incomplete orders
+        orders = get_orders(conn, cursor)
+
         return render_template('index.html', error=str(e), orders=orders)
     finally:
-        orders = get_orders(conn, cursor)
         cursor.close()
         conn.close()
-
-    return render_template('index.html', message='Order Created', orders=orders)
 
 @app.route('/sendsms', methods=['POST'])
 def send_sms():
     order_number = request.form['sms_ordernumber']
-    print('in send_sms: ',order_number)
+    orders = None
+
     try:
         conn, cursor = get_order_information(order_number)
         row = cursor.fetchone()
+        # phone number to send message to
         cell_phone_number = row[2]
-
         # when the message is not sent for that record the message_sent_time will be set to null
         message_sent_time = None
         random_number = randint(0, 100)
@@ -108,18 +119,24 @@ def send_sms():
             message_sent_time = now.strftime('%Y-%m-%d %H:%M:%S')
         cursor.execute('UPDATE ORDERS SET MESSAGE_SENT_TIME = %s WHERE ORDER_NUMBER= %s', (message_sent_time,order_number))
         conn.commit()
+        #send back incomplete orders
+        orders = get_orders(conn, cursor)
+
+        return render_template('index.html', message='Message Sent', orders=orders, sendsms_ordernumber=order_number)
     except Exception as e:
         logging.error(e)
-        return render_template('index.html', error=str(e))
+        #send back incomplete orders
+        orders = get_orders(conn, cursor)
+
+        return render_template('index.html', error=str(e), orders=orders)
     finally:
         cursor.close()
         conn.close()
 
-    return render_template('index.html', message='Message Sent')
-
 @app.route('/confirmpickup', methods=['POST'])
 def confirm_pickup():
     order_number = request.form['pickup_ordernumber']
+    orders = None
 
     try:
         conn, cursor = get_order_information(order_number)
@@ -131,13 +148,18 @@ def confirm_pickup():
         # once pickedup send survey question
         cell_phone_number = row[2]
         send_message(cell_phone_number, 'We hope you enjoyed your meal from Haldi! Please help us improve our service by rating your take-out order experience on a scale of 1-5, with 5 being the most positive.')
+        #send back incomplete orders
+        orders = get_orders(conn, cursor)
+
+        return render_template('index.html', message='Pickup confirmed', orders=orders)
     except Exception as e:
         logging.error(e)
-        return render_template('index.html', error=str(e))
+        #send back incomplete orders
+        orders = get_orders(conn, cursor)
+
+        return render_template('index.html', error=str(e), orders=orders)
     finally:
         cursor.close()
         conn.close()
-
-    return render_template('index.html', message='Pickup confirmed')
 
 
